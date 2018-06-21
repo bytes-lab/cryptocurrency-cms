9040d4d0d51d653b75f4b167c5626f81bd8158ec
IT-corridor <jason.5001001@gmail.com>
2018-06-22 02:35:09 +0530

    revise hist hour scraper for data type

--- scripts/hist_coinhourly_gecko.py
+++ scripts/hist_coinhourly_gecko.py
@@ -33,17 +33,17 @@ def main():
                 cg_coin = CoingeckoCoin.objects.get(id=coin.coingecko)
                 url_ = 'https://api.coingecko.com/api/v3/coins/{}/history?date={}&localization=false'.format(cg_coin.uid, single_date.strftime("%d-%m-%Y"))
                 info = requests.get(url_).json()
-                print (url_, '-----------------------')
+                # print (url_, '-----------------------')
                 # pdb.set_trace()
                 if not info.get('community_data'):
                     continue
-                print (coin.id, coin.symbol, single_date, '$$$$$$$$$$$$$$$$$$$$$')
+                # print (coin.id, coin.symbol, single_date, '$$$$$$$$$$$$$$$$$$$$$')
                 hour_info['facebook_likes'] = info.get('community_data')['facebook_likes']
                 hour_info['twitter_followers'] = info.get('community_data')['twitter_followers']
                 hour_info['reddit_subscribers'] = info.get('community_data')['reddit_subscribers']
                 hour_info['reddit_average_posts_48h'] = info.get('community_data')['reddit_average_posts_48h']
                 hour_info['reddit_average_comments_48h'] = info.get('community_data')['reddit_average_comments_48h']
-                hour_info['reddit_accounts_active_48h'] = info.get('community_data')['reddit_accounts_active_48h']
+                hour_info['reddit_accounts_active_48h'] = int(float(info.get('community_data')['reddit_accounts_active_48h'] or 0))
 
                 hour_info['repo_forks'] = info.get('developer_data')['forks']
                 hour_info['repo_stars'] = info.get('developer_data')['stars']
@@ -58,7 +58,7 @@ def main():
                 hour_info['date_of_entry'] = single_date
 
                 if hour_info:
-                    print (hour_info)
+                    # print (hour_info)
                     hour_info['coin_id'] = coin.id
                     CoinHourlyInfo.objects.create(**hour_info)
 
