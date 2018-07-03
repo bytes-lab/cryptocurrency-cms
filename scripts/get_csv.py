f468ae0be5c91478bdbd61d76fbb1364b473699f
IT-corridor <jason.5001001@gmail.com>
2018-07-03 15:48:29 +0530

    finish download cli

---
+++ scripts/get_csv.py
@@ -0,0 +1,16 @@
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = 'http://localhost:8000/get_csv?ex={}&timeframe={}&start={}&end={}'
+    if len(sys.argv) < 4:
+        print 'Please provide valid parameraters.\ne.g)$python get_csv.py binance 1min 1402197000 1402197180'
+        exit(0)
+
+    url = url.format(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    info = requests.get(url)
+    file_path = '{}-{}.csv'.format(sys.argv[1], sys.argv[3])
+
+    with open(file_path, "wb") as file:
+        file.write(info.content)
